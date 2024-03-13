8beca0250a5f9e97b16d340ae5b4d6b256851f94
Orivelton César <orivelton10@gmail.com>
2024-03-13 11:26:27 +0000

    Chore: Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Autocomplete } from "@/components/Autocomplete";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Autocomplete />
+    </main>
+  );
+}
---
+++ src/components/Autocomplete/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export function Autocomplete() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Command>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>Profile</CommandItem>
+            <CommandItem>Billing</CommandItem>
+            <CommandItem>Settings</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </QueryClientProvider>
+  );
+}
